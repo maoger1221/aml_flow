ac7f57b60e3ed49c6db9e0ffb5b3a16822e0b27b
maopengyu <maopengyu@4paradigm.com>
2019-06-05 18:05:06 +0800

    修复判断hdfs文件存在的逻辑

--- aml_flow.py
+++ aml_flow.py
@@ -17,6 +17,7 @@ import os
 import json
 import shutil
 from sklearn import metrics
+import logging
 
 args = {
     'owner': 'aml',
@@ -38,8 +39,8 @@ dag = DAG(
 #############################################
 # 全局变量
 # 获取配置文件中的参数
-sl_config = Variable.get('sl_config', deserialize_json=True, default_var={"hdfs_url":"http://172.27.128.237:50070","hdfs_user":"hdfs","daily_dir_list":["trx","str"],"static_dir_list":["retail","corporate","account"],"base_local_path":"/root/airflow/aml_data/sl_data/{}/","base_local_metrics_path":"/root/airflow/aml_data/sl_data/{}/for_metrics/","base_local_model_path":"/root/airflow/aml_data/model/{}","base_local_predict_res_path":"/root/airflow/aml_data/bp_data/res/{}","model_prefix":"he_test_xgboost","predict_res_prefix":"pred_full_table","base_remote_daily_path":"/anti-money/daily_data_group/{}/daily/{}","base_remote_static_path":"/anti-money/daily_data_group/{}/all","base_remote_model_path":"/anti-money/he_test/model/{}","base_remote_predict_res_path":"/anti-money/he_test/predict_res/{}","specified_model_path":"","start_time":"2018-05-01","end_time":"2018-05-27","metrics_start_time":"2018-05-28","metrics_end_time":"2018-05-30"})
-print('config: {}'.format(sl_config))
+sl_config = Variable.get('sl_config', deserialize_json=True, default_var={"hdfs_url":"localhost:50070","hdfs_user":"hdfs","daily_dir_list":["trx","str"],"static_dir_list":["retail","corporate","account"],"base_local_path":"/root/airflow/aml_data/sl_data/{}/","base_local_metrics_path":"/root/airflow/aml_data/sl_data/{}/for_metrics/","base_local_model_path":"/root/airflow/aml_data/model/{}","base_local_predict_res_path":"/root/airflow/aml_data/bp_data/res/{}","model_prefix":"he_test_xgboost","predict_res_prefix":"pred_full_table","base_remote_daily_path":"/anti-money/daily_data_group/{}/daily/{}","base_remote_static_path":"/anti-money/daily_data_group/{}/all","base_remote_model_path":"/anti-money/he_test/model/{}","base_remote_predict_res_path":"/anti-money/he_test/predict_res/{}","specified_model_path":"","start_time":"2018-05-01","end_time":"2018-05-27","metrics_start_time":"2018-05-28","metrics_end_time":"2018-05-30"})
+logging.info('config: {}'.format(sl_config))
 hdfs_url = sl_config['hdfs_url']
 hdfs_user = sl_config['hdfs_user']
 daily_dir_list = sl_config['daily_dir_list']
@@ -109,14 +110,14 @@ def download_daily_data_from_hdfs(base_local_path, start_time, end_time):
                 if not os.path.exists(base_local_path.format(sl_dir)):
                     os.makedirs(base_local_path.format(sl_dir))
                 try:
-                    print("downloading {} from {}".format(base_local_path.format(sl_dir), base_remote_daily_path.format(sl_dir, date)))
+                    logging.info("downloading {} from {}".format(base_local_path.format(sl_dir), base_remote_daily_path.format(sl_dir, date)))
                     # 这个client的download，目标本地路径/a/b/，远端路径/c/d/，当/a/b/存在时，下载后/a/b/d/；当/a/b/不存在时，下载后/a/b/
                     hdfs_client.download(base_remote_daily_path.format(sl_dir, date), base_local_path.format(sl_dir))
                 except HdfsError as e:
                     # 这个hdfs库无法判断文件是否存在，只能采用这种粗暴的方式
-                    print(base_remote_daily_path.format(sl_dir, date) + "下载文件出错:" + e.message)
+                    logging.info(base_remote_daily_path.format(sl_dir, date) + "下载文件出错:" + e.message)
             else:
-                print(base_local_path.format(sl_dir)+'{}/'.format(date) + "已存在，无需下载")
+                logging.info(base_local_path.format(sl_dir)+'{}/'.format(date) + "已存在，无需下载")
     return date_list
 
 
@@ -126,10 +127,10 @@ def download_static_data_from_hdfs(base_local_path):
             # clear_local_path(base_local_path.format(sl_dir))
             if not os.path.exists(base_local_path.format(sl_dir)):
                 os.makedirs(base_local_path.format(sl_dir))
-            print("downloading {} from {}".format(base_local_path.format(sl_dir), base_remote_static_path.format(sl_dir)))
+            logging.info("downloading {} from {}".format(base_local_path.format(sl_dir), base_remote_static_path.format(sl_dir)))
             hdfs_client.download(base_remote_static_path.format(sl_dir), base_local_path.format(sl_dir))
         else:
-            print(base_local_path.format(sl_dir) + "已存在，无需下载")
+            logging.info(base_local_path.format(sl_dir) + "已存在，无需下载")
 
 
 get_data_operator = PythonOperator(
@@ -173,24 +174,24 @@ def process_data_from_local(date_list, base_local_path):
         daily_dir_dict[sl_dir] = pd.DataFrame()
         for date in date_list:
             if os.path.exists(base_local_path.format(sl_dir) + date):
-                print("read daily table from path: {}".format(base_local_path.format(sl_dir) + date))
+                logging.info("read daily table from path: {}".format(base_local_path.format(sl_dir) + date))
                 daily_dir_dict[sl_dir] = daily_dir_dict[sl_dir].append(pd.read_parquet(base_local_path.format(sl_dir) + date))
     # 读取静态表
     static_dir_dict = {}
     for sl_dir in static_dir_list:
         # 初始化
-        print("read static table from path: {}".format(base_local_path.format(sl_dir) + 'all'))
+        logging.info("read static table from path: {}".format(base_local_path.format(sl_dir) + 'all'))
         static_dir_dict[sl_dir] = pd.read_parquet(base_local_path.format(sl_dir) + 'all', engine='auto')
     # 拼表
     # 这里还是写死了，讨厌
     str_with_trx = pd.merge(daily_dir_dict['str'], daily_dir_dict['trx'], how='left', on=['trx_id'])
-    print("merged str_with_trx")
+    logging.info("merged str_with_trx")
     str_with_trx_with_retail = pd.merge(str_with_trx, static_dir_dict['retail'], how='left', on=['cust_id'])
-    print("merged str_with_trx_with_retail")
+    logging.info("merged str_with_trx_with_retail")
     str_with_trx_with_retail_with_corporate = pd.merge(str_with_trx_with_retail, static_dir_dict['corporate'], how='left', on=['cust_id'])
-    print("merged str_with_trx_with_retail_with_corporate")
+    logging.info("merged str_with_trx_with_retail_with_corporate")
     str_with_trx_with_retail_with_corporate_with_account = pd.merge(str_with_trx_with_retail_with_corporate, static_dir_dict['account'], how='left', on=['account_id'])
-    print("merged str_with_trx_with_retail_with_corporate_with_account")
+    logging.info("merged str_with_trx_with_retail_with_corporate_with_account")
     # 对类别特征做Label Encoder
     le = LabelEncoder()
     # labeled_data = str_with_trx_with_retail_with_corporate_with_account.copy()
@@ -231,14 +232,14 @@ def train_model(**kwargs):
               'nthread': 8,
               'silent': 1}
     bst = xgb.train(params, dtrain, num_boost_round=1000, evals=watchlist)
-    clear_local_path(local_model_path)
+    clear_local_path(base_local_model_path.format(''))
     bst.save_model(local_model_path)
     # 第二个参数为False，当文件不存在时return none，文件存在时返回文件信息
-    if hdfs_client.status(uploaded_model_path, False):
+    if not hdfs_client.status(uploaded_model_path, False):
         hdfs_client.upload(uploaded_model_path, local_model_path)
-        print('uploaded local model {} to {} successfully'.format(local_model_path, uploaded_model_path))
+        logging.info('uploaded local model {} to {} successfully'.format(local_model_path, uploaded_model_path))
     else:
-        print('{}已存在'.format(uploaded_model_path))
+        logging.info('{}已存在'.format(uploaded_model_path))
 
 
 train_model_operator = PythonOperator(
@@ -256,9 +257,9 @@ def get_metrics(**kwargs):
     X, y, str_with_trx_with_retail_with_corporate_with_account = process_data_from_local(date_list_for_metrics, base_local_metrics_path)
 
     dtrain = xgb.DMatrix(X)
-    clear_local_path('/root/airflow/aml_data/model/he_test_xgboost.model')
-    modle = xgb.Booster(model_file='/root/airflow/aml_data/model/he_test_xgboost.model')
-    y_pred = modle.predict(dtrain)
+    clear_local_path('/root/airflow/aml_data/model/')
+    model = xgb.Booster(model_file='/root/airflow/aml_data/model/he_test_xgboost.model')
+    y_pred = model.predict(dtrain)
     y_pred_binary = (y_pred >= 0.5) * 1
 
     auc = metrics.roc_auc_score(y, y_pred)
@@ -267,11 +268,11 @@ def get_metrics(**kwargs):
     F1_score = metrics.f1_score(y, y_pred_binary)
     Precesion = metrics.precision_score(y, y_pred_binary)
 
-    print('AUC: %.4f' % auc)
-    print('ACC: %.4f' % acc)
-    print('Recall: %.4f' % recall)
-    print('F1-score: %.4f' % F1_score)
-    print('Precesion: %.4f' % Precesion)
+    logging.info('AUC: %.4f' % auc)
+    logging.info('ACC: %.4f' % acc)
+    logging.info('Recall: %.4f' % recall)
+    logging.info('F1-score: %.4f' % F1_score)
+    logging.info('Precesion: %.4f' % Precesion)
     metrics.confusion_matrix(y, y_pred_binary)
 
     res_dict = {}
@@ -284,16 +285,16 @@ def get_metrics(**kwargs):
     res_json = json.dumps(res_dict)
 
     local_model_meta_path = base_local_model_path.format('{}.meta.json'.format(model_prefix))
-    clear_local_path(local_model_meta_path)
+    clear_local_path(base_local_model_path.format(''))
     with open(local_model_meta_path, "w") as f:
         f.write(res_json)
 
     # 第二个参数为False，当文件不存在时return none，文件存在时返回文件信息
-    if hdfs_client.status(base_remote_model_path.format(model_id) + '.meta.json', False):
+    if not hdfs_client.status(base_remote_model_path.format(model_id) + '.meta.json', False):
         hdfs_client.upload(base_remote_model_path.format(model_id) + '.meta.json', local_model_meta_path)
-        print('uploaded local meta {} to {} successfully'.format(local_model_meta_path, base_remote_model_path.format( model_id) + '.meta.json'))
+        logging.info('uploaded local meta {} to {} successfully'.format(local_model_meta_path, base_remote_model_path.format( model_id) + '.meta.json'))
     else:
-        print('{}已存在'.format(base_remote_model_path.format(model_id) + '.meta.json'))
+        logging.info('{}已存在'.format(base_remote_model_path.format(model_id) + '.meta.json'))
 
 
 get_metrics_operator = PythonOperator(
@@ -350,15 +351,15 @@ def predict(**kwargs):
 
     predict_res_name = predict_res_prefix + today + '.csv'
 
-    clear_local_path(base_local_predict_res_path.format(predict_res_name))
+    clear_local_path(base_local_predict_res_path.format(''))
     pred_full_table.to_csv(base_local_predict_res_path.format(predict_res_name))
 
     # 第二个参数为False，当文件不存在时return none，文件存在时返回文件信息
-    if hdfs_client.status(base_remote_predict_res_path.format(predict_res_name), False):
+    if not hdfs_client.status(base_remote_predict_res_path.format(predict_res_name), False):
         hdfs_client.upload(base_remote_predict_res_path.format(predict_res_name), base_local_predict_res_path.format(predict_res_name))
-        print('uploaded predict res {} to {} successfully'.format(base_local_predict_res_path.format(predict_res_name), base_remote_predict_res_path.format(predict_res_name)))
+        logging.info('uploaded predict res {} to {} successfully'.format(base_local_predict_res_path.format(predict_res_name), base_remote_predict_res_path.format(predict_res_name)))
     else:
-        print('{}已存在'.format(base_remote_predict_res_path.format(predict_res_name)))
+        logging.info('{}已存在'.format(base_remote_predict_res_path.format(predict_res_name)))
 
 
 predict_operator = PythonOperator(
